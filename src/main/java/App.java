9312f95230f3d0e011b7972e315287835885ada5
Kayl Eubanks <kayleubanks@gmail.com>
2018-05-25 15:12:14 -0700

    Add functionality for user to select thier beverage option

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -39,6 +39,19 @@ public class App {
                 newEvent.setFoodOption(userFoodChoice);
                 System.out.println("Great! We will be serving you and your guests " + newEvent.getFoodOption());
 
+                System.out.println("Next, please what drinks we should serve. The options include: water, soda, beer, wine, or open bar for a premium price.");
+                System.out.print("Your selection of beverage: ");
+                String userBeverageChoice = bufferedReader.readLine().toLowerCase();
+                while (newEvent.checkBeverageOption(userBeverageChoice) == false) {
+                    System.out.println("Sorry, that is an invalid selection. Please enter one of the five choices (water, beer, wine, soda, open bar)");
+                    System.out.print("Your selection: ");
+                    userBeverageChoice = bufferedReader.readLine().toLowerCase();
+                }
+                newEvent.setBeverageOption(userBeverageChoice);
+                System.out.println("Got it. We will be serving " + newEvent.getBeverageOption());
+
+                System.out.println("Finally, let's decide on entertainment. We offer a limited selection of musical entertainment. We options for a live DJ, a live band, our own set playlist or a custom playlist we can tailor to your specific taste.");
+                System.out.println("Please enter one of the following as your selection: live DJ, live band, set playlist, or custom playlist");
 
             } catch (IOException e) {
                 e.printStackTrace();
